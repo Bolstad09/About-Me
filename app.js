a1e093befca2a91e6ab82f15ef6efb90a395c019
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-01-11 20:29:59 -0800

    js completed for now

---
+++ app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var userPoints = 0;
+
+console.log('Well, hello there');
+
+alert('Welcome to my trivia game to learn more about me, you can earn up to 5 points here.');
+// ---------------------FOR MY OWN USE -------------------------------
+var user = prompt('Before I share information about me, what is your name?');
+
+alert('Welcome ' + user + '! Very nice to meet you, lets go ahead and get started!');
+// ---------------------FOR MY OWN USE -------------------------------
+// var beginningStatement = alert('Alright ' + user + ', here we go!').toLowerCase();
+
+// console.log(beginningStatement);
+// // if (beginningStatement === 'yes' || beginningStatement === 'y'){
+// //     else {
+// //     alert('Okay, I\'ll wait...');
+// ---------------------FOR MY OWN USE -------------------------------
+var answer = prompt('First of all, do you think I\'m obsessed with dogs?' , 'type Yes or No').toLowerCase();
+    userPoints++;
+
+console.log(answer);
+
+//now we need to check if the users answer is correct and this is how we do it..
+//we also need to find out what is correct
+//find out what the answer is
+//we need to use toLowerCase or toUpperCase
+//we want to use a confirm box
+
+if (answer === 'yes' || answer === 'y'){
+    //if it's correct give them a point
+    userPoints+ 1;
+} else {
+    // if it's not correct, tell them to try again
+    alert('I can\'t believe you forgot, I told you that I was on the first day we met!');
+}
+
+// ---------------------FOR MY OWN USE -------------------------------
+var answer2= prompt(user + ', do you think my dog\'s name is Na\'kita?' , 'type Yes or No').toUpperCase();
+    userPoints+ 1;
+
+console.log(answer2);
+
+if (answer2 === 'yes' || 'y'){
+    userPoints+ 1;
+} else{
+    alert('Sorry, that\'s incorrect, on to the next question');
+}
+// ---------------------FOR MY OWN USE -------------------------------
+var answer3= prompt('Do I like cats?' , 'type Yes or No').toUpperCase();
+    userPoints++;
+
+    console.log(answer3);
+
+if (answer3 === 'no' || 'n'){
+    userPoints- 1;
+} else{
+    alert('You\'re correct!');} 
+// ---------------------FOR MY OWN USE -------------------------------
+var answer4= prompt('Do I like pizza?' , 'type Yes or No').toUpperCase();
+    userPoints++;
+
+    console.log(answer4);
+
+if (answer4 === 'yes' || 'y'){
+    userPoints+ 1;
+}
+// ---------------------FOR MY OWN USE -------------------------------
+var answer5= prompt(user + ', Do I like wine? ' , 'type Yes or No').toUpperCase();
+    userPoints++;
+
+    console.log(answer5);
+
+if (answer5 === 'yes' || 'y'){
+    userPoints++;
+} else{
+    alert('Sorry, that\'s incorrect, let\'s see what your score is!');
+}
+// ---------------------FOR MY OWN USE -------------------------------
+alert('you have ' + userPoints + ' points');
+
+
+
+
+
+
+
+
